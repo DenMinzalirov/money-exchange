8262979a9141b9edb4d99dcd1372f67bff7eb656
Den Minzakirov <Den.Minzakirov@gmail.com>
2019-03-02 23:14:28 +0300

    inpliment function makeExchange

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+  let result = {};
+  let balance = 0;
+
+  if (currency > 10000) {
+    result.error =
+      "You are rich, my friend! We don't have so much coins for exchange";
+  } else if (currency < 0) {
+    result = {};
+  } else {
+    let H = (currency - (currency % 50)) / 50;
+    balance = currency - H * 50;
+    if (H != 0) result.H = H;
+
+    let Q = (balance - (balance % 25)) / 25;
+    balance = balance - Q * 25;
+    if (Q != 0) result.Q = Q;
+
+    let D = (balance - (balance % 10)) / 10;
+    balance = balance - D * 10;
+    if (D != 0) result.D = D;
+
+    let N = (balance - (balance % 5)) / 5;
+    balance = balance - N * 5;
+    if (N != 0) result.N = N;
+
+    let P = balance;
+    if (P != 0) result.P = P;
+  }
+  return result;
+};
